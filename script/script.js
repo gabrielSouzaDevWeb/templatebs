2be2de350bd51205eda5f5788380996a520aac6e
Gabriel Lucas de Souza <gabrielsouzadevweb@gmail.com>
2020-07-11 12:44:25 -0300

    dual color nav_bar, initial --desktop--

--- script/script.js
+++ script/script.js
@@ -2,14 +2,28 @@ function index() {
 
     var menu_bar = document.querySelector(".btn-menu-svg");
     var menu_mobile= document.querySelector(".menu-mobile");
+    var nav_bar= document.querySelector(".nav-bar");
+    var header_bg= document.querySelector(".header");
 
     menu_bar.addEventListener("click",function(){
        toggleDisplay(menu_mobile, "flex"); 
-       
     });
 
     window.addEventListener("resize",function(){
         toCompareWidth(menu_bar);
+    });
+
+    window.addEventListener("onload",function(){ nav_bar.style.backgroundColor = document.defaultView.getComputedStyle(header_bg, null).backgroundColor});
+
+    window.addEventListener("scroll", function() {
+        var getStyle = document.defaultView.getComputedStyle(header_bg, null).backgroundColor;
+        if(window.scrollY >= 340 && widthDisplay() >= 600){
+            nav_bar.style.backgroundColor = "#151515";
+            
+            
+        }else{
+            nav_bar.style.backgroundColor = getStyle;
+        }
     });
 
     function toggleDisplay(tagToggle,displayValue){//documentar function e trocar nome
@@ -25,16 +39,13 @@ function index() {
     function toCompareWidth(item){
         if (widthDisplay() <= 600) {
             item.style.display = "block";
-            console.log("<");
         } else{
             item.style.display = "none";
             menu_mobile.style.display="none";
-            console.log(">");
         }
     }
 
     function widthDisplay() {
-        
         if (window.innerWidth){
             return  window.innerWidth;
         } else if(document.documentElement && document.documentElement.clientWidth){
@@ -43,6 +54,13 @@ function index() {
             return document.body;
         }
     }
+
+    /*function getStyle(tagName,style) {
+        var getStyle= document.defaultView.getComputedStyle(tagName, null).style;
+        var unitTest = console.log("getStyle");
+        return getStyle, unitTest;
+        
+    }*/
 
 }
 
